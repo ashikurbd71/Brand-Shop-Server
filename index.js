0450448bd5c0894fff245b1d8db0ba7f3d6ba6cb
ashikurbd71 <ashikurovi2003@gmail.com>
2023-10-17 21:16:34 +0600

    brand sever git push'

---
+++ index.js
@@ -0,0 +1,54 @@
+
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT ||  5000
+
+// midleware
+
+app.use(cors())
+app.use(express.json())
+
+// brandshopbd71
+// brandshop1233
+
+
+
+// data
+
+
+
+const uri = `mongodb+srv://brandshopbd71:brandshop1233@cluster0.mtnypra.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
